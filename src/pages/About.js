3419508be3feb33e655f776f577799cea22f9d29
Alex Carlson <alexcarlson@Alexs-MacBook-Air.local>
2020-11-08 11:43:15 -0600

    second

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About</h1>
+      <p>
+      </p>
+    </section>
+  )
+}
+
+export default About
